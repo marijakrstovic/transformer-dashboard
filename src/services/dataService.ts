2deb73bdc9de47d1b92a5759e59b9dda882512f1
Marija <marija@fahecom.com>
2025-03-21 16:34:54 +0400

    added mocky.io to store data and simulate fetch

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,7 +1,6 @@
 import type { Transformer } from '../types/types';
 
-// load fallback data just in case mocky.io is down
-const API_URL = import.meta.env.VITE_API_URL ?? '/data/sampledata.json';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchTransformers(): Promise<Transformer[]> {
   const res = await fetch(API_URL);
